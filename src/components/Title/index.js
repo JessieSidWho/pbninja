429f67c95b89f1178653ebf6863984f1e76c172c
Jessie Sidhu <jsidhu22@gmail.com>
2019-08-19 19:14:06 -0700

    fixed phone number button

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -17,7 +17,7 @@ const Title = () => {
                         <Nav.Link className='p-3 ' href="#about" style={{color: 'orangered'}}>About Us</Nav.Link>
                         <Nav.Link className='p-3 ' href="#pricing" style={{color: 'orangered'}}>Pricing</Nav.Link>
                         <Nav.Link className='p-3 ' href="#contact" style={{color: 'orangered'}}>Contact</Nav.Link>
-                        <Nav.Link id='callUs' className='ml-2'><button id='callUs' className='text-white btn btn-primary rounded-lg' href='tel:6195047147' >Call Us <i class="fas fa-phone"></i></button></Nav.Link>
+                        <Nav.Link id='callUs' className='ml-2'><a href='tel:6195047147'><button id='callUs' className='text-white btn btn-primary rounded-lg'  >Call Us <i class="fas fa-phone"></i></button></a></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
