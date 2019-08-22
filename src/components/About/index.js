84634a07918ebeaee63506ce1b5b1d71d573a041
Jessie Sidhu <jsidhu22@gmail.com>
2019-08-22 11:22:38 -0700

    progress

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './about.css';
-import Boxes from '../../images/package.jpg';
+// import Boxes from '../../images/package.jpg';
 import Props from '../../images/props.png';
 import Zoom from 'react-reveal/Zoom';
 import Rotate from 'react-reveal/Rotate';
@@ -8,14 +8,14 @@ import Bounce from 'react-reveal/Bounce';
 
 const About = () => {
     return (
-        <div id='about' className='row justify-content-md-center mx-auto pt-5' style={{ zIndex: '5' }}>
-            <div id='' className='col-md-12 mx-auto mt-5 mb-5 rounded-lg'>
-                <Bounce delay='500' top>
+        <div id='about' className='row justify-content-md-center mx-auto ' style={{ zIndex: '5' }}>
+            <div id='' className='col-md-12 mx-auto mb-5 rounded-lg'>
+                <Bounce delay='2200' top>
                     <h1 className='text-center' style={{ fontFamily: 'Comfortaa', fontSize: '50px' }}>What we Offer</h1>
                 </Bounce>
                 <div className='row justify-content-md-center'>
-                    <div className='col-md-8 mx-auto pl-5 pr-5 pb-5 pt-3' style={{ fontSize: '20px' }} >
-                        <Rotate delay='700' bottom right>
+                    <div className='col-md-8 mx-auto pl-5 pr-5 pb-5 ' style={{ fontSize: '20px' }} >
+                        <Rotate delay='2500' bottom right>
                             <p style={{ fontSize: '30px' }}>
                                 Are You Ready For Your Closeup?
                         </p>
@@ -48,9 +48,9 @@ const About = () => {
                 </div>
 
                 <Zoom delay='1000' bottom>
-                    <div id='amenities' className='row justify-content-md-center mt-4'>
+                    <div id='amenities' className='row justify-content-md-center'>
                         <div className='col-md-12 text-center'>
-                            <img id="gallery" className='' src={Boxes} alt='placeholder' height='100%' width='100%' />
+                            {/* <img id="gallery" className='' src={Boxes} alt='placeholder' height='80%' width='80%' /> */}
                         </div>
                     </div>
                 </Zoom>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import './home.css';
-import Logo from './../../images/pb.jpg';
+// import Logo from './../../images/pb.jpg';
 import Flip from 'react-reveal/Flip';
-import Jump from 'react-reveal/Jump';
+// import Jump from 'react-reveal/Jump';
 
 
 const Home = () => {
     return (
         <>
-            <div id='home' className='row-fluid justify-content-md-center'>
+        <Flip delay='1000' top>
+            <div id='home' className='row-fluid justify-content-md-center' style={{}}>
                 <div className='col-md-12 mx-auto text-center' style={{ position: 'fixed', zIndex: '-1' }}>
-                    <Flip delay='1000' top>
-                        <img id='imgID' className='' src={Logo} alt='logo' style={{ height: '60%', width: '60%' }} />
-                    </Flip>
+                    
+                        {/* <img id='imgID' className='' src={Logo} alt='logo' style={{ height: '60%', width: '60%' }} /> */}
+                    
                 </div>
             </div>
+        </Flip>
 
             <div id='arrow' className='row justify-content-md-center'>
                 <div className='col-md-12 mx-auto text-center' style={{ position: 'fixed', zIndex: '-1' }}>
                     <div>
-                        <Jump delay='2000' duration='2000'>
+                        {/* <Jump delay='2000' duration='2000'>
                         <i className=" fas fa-angle-double-down"></i>
-                        </Jump>
+                        </Jump> */}
                     </div>
                 </div>
             </div>
