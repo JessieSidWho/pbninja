997fda1cbfae1c13be170486e64171e3655acf1b
Jessie Sidhu <jsidhu22@gmail.com>
2019-08-24 14:44:19 -0700

    added logo in navbar

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './title.css';
+import Logo from '../../images/pb.jpg';
 
 
 const Title = () => {
     return (
         <>
             <Navbar className='row' collapseOnSelect fixed='top' expand="lg" bg="white" variant="white">
-                <Navbar.Brand className='' href="#home"></Navbar.Brand>
+                
                 <Navbar.Toggle className='mr-auto pl-4 pr-0 rounded-lg fas fa-bars' aria-controls="responsive-navbar-nav"/>
-                    
+                <Navbar.Brand className='' href="#home"><img id='navLogo' className='ml-5' src={Logo} alt='' height='55px'/></Navbar.Brand>  
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Link className='p-3 animated bounce delay-2s' href="#home" style={{color: 'orangered'}}>Home</Nav.Link>
